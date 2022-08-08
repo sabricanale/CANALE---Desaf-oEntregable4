63dc006095a7d1e9781dd05d2e4b1f0596bdebcf
sabricanale <sabrii.canale@gmail.com>
2022-08-08 20:48:19 -0300

    Cambios Finales!

--- index.js
+++ index.js
@@ -1,35 +1,92 @@
-let estudiantes = [];
-let formulario = document.getElementById("formulario");
+//DECLARACION E INICIALIZACIÓN DE VARIABLES
+let estudiantes = []
+let formulario = document.getElementById("formulario")
 let nombreEstudiante = document.getElementById("nombreEstudiante")
 let inputNota1 = document.getElementById("inputNota1")
 let inputNota2 = document.getElementById("inputNota2")
 let inputNota3 = document.getElementById("inputNota3")
 let tabla = document.getElementById("tablaProductos")
 let btnMostrarAlert = document.getElementById("mostrarAlert")
+const botonEnviarLista = document.getElementById("enviarLista")
 
+//CLASE ESTUDIANTE
 class Estudiante {
     constructor(nombre, nota1, nota2, nota3){
-        this.nombre = nombre;
-        this.nota1 = nota1;
-        this.nota2 = nota2;
-        this.nota3 = nota3;
-        this.promedio = (nota1 + nota2 + nota3) / 3;
-        this.resultado;
-        this.calcularResultado();
+        this.nombre = nombre
+        this.nota1 = nota1
+        this.nota2 = nota2
+        this.nota3 = nota3
+        this.promedio = (nota1 + nota2 + nota3) / 3
+        this.resultado
+        this.calcularResultado()
     }
     calcularResultado() {
-        this.resultado = this.promedio >= 6 ? "APROBÓ" : "NO APROBÓ"; 
+        this.resultado = this.promedio >= 6 ? "APROBÓ" : "NO APROBÓ" 
     }
 }
 
-// ENVIO DE MAILS
-const botonEnviarLista = document.getElementById("enviarLista");
+//AGREGA ESTUDIANTES A LA TABLA
+function agregarEstudiantesTabla(){
+    estudiantes.forEach((estudiante) => {let filaTabla = document.createElement("tr")
+        filaTabla.innerHTML = `
+        <td>${estudiante.nombre}</td>
+        <td>${estudiante.nota1}</td>
+        <td>${estudiante.nota2}</td>
+        <td>${estudiante.nota3}</td>
+        <td>${estudiante.promedio}</td>
+        <td>${estudiante.resultado}</td>
+        ` 
+        tabla.tBodies[0].append(filaTabla)
+    });
+}
+//VALIDA VALORES INGRESADOS
+function validarFormulario(){
+    let nombre = nombreEstudiante.value;
+    if(nombre == "") {
+        Swal.fire({icon: 'error', title: `No ingresaste nombre`});
+        return false;
+    }
+    let nota1 = parseFloat(document.getElementById("inputNota1").value);
+    if(isNaN(nota1) || nota1 > 10 || nota1 < 0){
+        Swal.fire({icon: 'error', title: `Error al ingresar la primera nota`});
+        return false;
+    }
+    let nota2 = parseFloat(document.getElementById("inputNota2").value);
+    if(isNaN(nota2) || nota2 > 10 || nota2 < 0){
+        Swal.fire({icon: 'error', title: `Error al ingresar la segunda nota`});
+        return false;
+    }
+    let nota3 = parseFloat(document.getElementById("inputNota3").value);
+    if(isNaN(nota3) || nota3 > 10 || nota3 < 0){
+        Swal.fire({icon: 'error', title: `Error al ingresar la tercera nota`});
+        return false;
+    }
+    return true;        
+}
+
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let formularioValidado = validarFormulario();
+    let nombre = nombreEstudiante.value
+    let nota1 = parseFloat(document.getElementById("inputNota1").value)
+    let nota2 = parseFloat(document.getElementById("inputNota2").value)
+    let nota3 = parseFloat(document.getElementById("inputNota3").value)
+    if(formularioValidado) {
+        let estudiante = new Estudiante(nombre, nota1, nota2, nota3)
+        estudiantes.push(estudiante);
+        actualizarTabla();
+        btnMostrarAlert.onclick = mostrarAlert()
+        formulario.reset();
+    }
+})
+
+// ENVIO DE MAIL
 botonEnviarLista.addEventListener("click", () => {
     enviarCorreoConLista();
 })
 
 function enviarCorreoConLista() {
-
     const serviceID = 'service_9hhchxf';
     const templateID = 'template_bsey3b6';
     let dataToSend = localStorage.getItem("listaEstudiantes");
@@ -49,71 +106,26 @@ function enviarCorreoConLista() {
         data: JSON.stringify(data),
         contentType: 'application/json'
     }).done(function() {
-        Swal.fire({title: `La lista fue enviada a su correo`})
+        Swal.fire({
+            title: '¿Estás seguro de enviar lista?',
+            text: "La lista se enviará automáticamente al correo de la Institución",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Enviar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Lista enviada',
+                'La lista de estudiantes fue enviada al correo de la Institución',
+                'success'
+              )
+            }
+          })
     }).fail(function(error) {
         alert('Error' + JSON.stringify(error));
     });
-}
-
-function actualizarTabla() {
-    while(tabla.rows.length > 1){
-        tabla.deleteRow(1)
-    }
-    agregarEstudiantesTabla();
-    renovarLocalStorage();
-}
-function agregarEstudiantesTabla(){
-    estudiantes.forEach((estudiante) => {let filaTabla = document.createElement("tr")
-        filaTabla.innerHTML = `
-        <td>${estudiante.nombre}</td>
-        <td>${estudiante.nota1}</td>
-        <td>${estudiante.nota2}</td>
-        <td>${estudiante.nota3}</td>
-        <td>${estudiante.promedio}</td>
-        <td>${estudiante.resultado}</td>
-        ` 
-        tabla.tBodies[0].append(filaTabla)
-    });
-}
-
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let formularioValidado = validarFormulario();
-    if(formularioValidado) {
-        let estudiante = new Estudiante(
-            nombreEstudiante.value, 
-            parseFloat(document.getElementById("inputNota1").value),
-            parseFloat(document.getElementById("inputNota2").value),
-            parseFloat(document.getElementById("inputNota3").value)
-        )
-        estudiantes.push(estudiante);
-        actualizarTabla();
-        formulario.reset();
-    }
-})
-
-function validarFormulario(){
-    let nombre = nombreEstudiante.value;
-    if(nombre == "") {
-        Swal.fire({title: `No ingresaste nombre`});
-        return false;
-    }
-    let nota1 = parseFloat(document.getElementById("inputNota1").value);
-    if(isNaN(nota1) || nota1 > 10 || nota1 < 0){
-        Swal.fire({title: `Error al ingresar la primera nota`});
-        return false;
-    }
-    let nota2 = parseFloat(document.getElementById("inputNota2").value);
-    if(isNaN(nota2) || nota2 > 10 || nota2 < 0){
-        Swal.fire({title: `Error al ingresar la segunda nota`});
-        return false;
-    }
-    let nota3 = parseFloat(document.getElementById("inputNota3").value);
-    if(isNaN(nota3) || nota3 > 10 || nota3 < 0){
-        Swal.fire({title: `Error al ingresar la tercera nota`});
-        return false;
-    }
-    return true;    
 }
 
 function renovarLocalStorage(){
@@ -133,7 +145,15 @@ function mostrarAlert(){
     })
 }
 
-function main (){    
+function actualizarTabla() {
+    while(tabla.rows.length > 1){
+        tabla.deleteRow(1)
+    }
+    agregarEstudiantesTabla();
+    renovarLocalStorage();
+}
+
+function main (){   
     obtenerProductosLocalStorage()
     agregarEstudiantesTabla()
 }
